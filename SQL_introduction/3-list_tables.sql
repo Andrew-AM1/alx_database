39088149aaa23902a381a6d7686dc1a571c30cd1
“Andrew-AM1” <andrewamoako1@gmail.com>
2023-08-17 21:04:48 +0000

    Write a script that lists all the tables of a database

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,5 +1,5 @@
 -- a script that lists all the tables of a database in your MySQL server
 
-SELECT table_name AS Tables_in_mysql
+SELECT table_name AS Tables_in_hbtn_test_db_0
 FROM information_schema.tables
 WHERE table_schema = DATABASE();
